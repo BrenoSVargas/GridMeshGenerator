73bf3149754fab418105532dfcf7b431b65897b5
Breno <breno_fari@hotmail.com>
2021-12-13 21:40:02 -0300

    Grid and Mesh

---
+++ Assets/_Scripts/GridBase/Testing.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Testing : MonoBehaviour
+{
+    private GridLogic grid;
+
+    public Vector2Int sizeGrid;
+    public float cellSize;
+    public Vector3 originGrid;
+    private void Start()
+    {
+        grid = new GridLogic(sizeGrid.x, sizeGrid.y, cellSize, originGrid);
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePos = MouseScreenPosition();
+            grid.SetValue(mousePos, 56);
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            Debug.Log(grid.GetValue(MouseScreenPosition()));
+        }
+    }
+
+    private Vector3 MouseScreenPosition()
+    {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePos.z = 0;
+        return mousePos;
+    }
+}
---
+++ Assets/_Scripts/Heatmap/TestHeatmap.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestHeatmap : MonoBehaviour
+{
+    private void Start()
+    {
+        GridLogic grid = new GridLogic(20, 10, 5f, Vector3.zero);
+    }
+}
